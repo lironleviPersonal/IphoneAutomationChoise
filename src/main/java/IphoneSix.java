adcf75e37dec745a1cdb37fb00954d559738c40c
lironlevi <levyliron123@gmail.com>
2019-06-10 22:46:42 +0300

    version 1.1

---
+++ src/main/java/IphoneSix.java
@@ -0,0 +1,13 @@
+public class IphoneSix {
+    private String name;
+    private double price;
+
+    public IphoneSix(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public String getName() {return this.name;}
+}
