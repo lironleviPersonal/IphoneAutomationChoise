a200d3995ff7b0ca89895184c9244202763d2de5
levyliron <levyliron123@gmail.com>
2019-06-15 15:57:54 +0300

    Add logs

---
+++ src/main/java/Logs.java
@@ -0,0 +1,18 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+
+import java.io.IOException;
+
+public class Logs {
+
+    /* Get actual class name to be printed on */
+    private static Logger log = LogManager.getLogger(Logs.class);
+
+    public static void information(String info){
+        log.info(info);
+    }
+    public static void debugging (String debug) {log.debug(debug);}
+}
